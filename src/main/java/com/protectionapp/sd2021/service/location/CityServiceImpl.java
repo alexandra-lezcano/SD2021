ce8fca3fcdcd1600012886541d1085d36a50cd85
alexandra-lezcano <alexandra.lezcano@fiuni.edu.py>
2021-11-10 09:37:12 -0300

    Commit#6-city service

---
+++ src/main/java/com/protectionapp/sd2021/service/location/CityServiceImpl.java
@@ -0,0 +1,42 @@
+package com.protectionapp.sd2021.service.location;
+
+
+import com.protectionapp.sd2021.dao.location.ICityDao;
+import com.protectionapp.sd2021.domain.location.CityDomain;
+import com.protectionapp.sd2021.dto.localization.CityDTO;
+import com.protectionapp.sd2021.dto.localization.CityResult;
+import com.protectionapp.sd2021.service.base.BaseServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CityServiceImpl extends BaseServiceImpl<CityDTO, CityDomain, CityResult> implements ICityService {
+    @Autowired
+    private ICityDao cityDao;
+
+    @Override
+    protected CityDTO convertDomainToDto(CityDomain domain) {
+        return null;
+    }
+
+    @Override
+    protected CityDomain convertDtoToDomain(CityDTO dto) {
+        return null;
+    }
+
+    @Override
+    public CityDTO save(CityDTO dto) {
+        return null;
+    }
+
+    @Override
+    public CityDTO getById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public CityResult getAll(Pageable pageable) {
+        return null;
+    }
+}

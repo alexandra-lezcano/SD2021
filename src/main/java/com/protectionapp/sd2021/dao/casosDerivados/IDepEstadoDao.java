a53be2b331b0bb9ffc7fed1f4a73ee9eadc42ec7
DalilaC33 <dalila.castelnovo@fiuni.edu.py>
2021-11-11 08:40:41 -0300

    DaoDepEstado #

---
+++ src/main/java/com/protectionapp/sd2021/dao/casosDerivados/IDepEstadoDao.java
@@ -0,0 +1,18 @@
+package com.protectionapp.sd2021.dao.casosDerivados;
+
+import com.protectionapp.sd2021.domain.casosDerivados.DepEstadoDomain;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+    @Repository
+    public interface IDepEstadoDao extends CrudRepository<DepEstadoDomain, Integer> {
+
+        public Page<DepEstadoDomain> findAll(Pageable pageable);
+
+
+
+}

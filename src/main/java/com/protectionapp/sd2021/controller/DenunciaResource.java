8033cc867e010488a78ffa67072f0aff42097b4b
SoyReni <caballeroreni@gmail.com>
2021-11-11 18:55:39 -0300

    Commit #6.5 - Reni - Cambios en el controlador de denuncia

--- src/main/java/com/protectionapp/sd2021/controller/DenunciaResource.java
+++ src/main/java/com/protectionapp/sd2021/controller/DenunciaResource.java
@@ -1,13 +1,17 @@
 package com.protectionapp.sd2021.controller;
 
 
+import com.protectionapp.sd2021.dao.denuncia.IDenunciaDao;
 import com.protectionapp.sd2021.dto.denuncia.DenunciaDTO;
 import com.protectionapp.sd2021.dto.denuncia.DenunciaResult;
 import com.protectionapp.sd2021.service.denuncia.DenunciaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/denuncia")
@@ -26,9 +30,16 @@ public class DenunciaResource {
         return denunciaService.getAll(PageRequest.of(num,5));
     }
 
-    /*
+    /*Post en construccion*/
     @PostMapping(   path="denuncia",
                     consumes = MediaType.APPLICATION_JSON_VALUE,
                     produces = MediaType.APPLICATION_JSON_VALUE)
-    public DenunciaDTO postDenuncia(@RequestBody DenunciaDTO)*/
+    public DenunciaDTO postDenuncia(@RequestBody DenunciaDTO denunciaDTO) {
+        return denunciaService.save(denunciaDTO);
+    }
+
+   
+
+
+
 }

4215d9b216bbb99656a622b53537b02168bb61c9
alexandra-lezcano <alexandra.lezcano@fiuni.edu.py>
2021-11-14 09:47:03 -0300

    Alex-DenunciaEstado

---
+++ src/main/java/com/protectionapp/sd2021/service/denuncia/IDenunciaEstadoService.java
@@ -0,0 +1,8 @@
+package com.protectionapp.sd2021.service.denuncia;
+
+import com.protectionapp.sd2021.dto.denuncia.DenunciaEstadoDTO;
+import com.protectionapp.sd2021.dto.denuncia.DenunciaEstadoResult;
+import com.protectionapp.sd2021.service.base.IBaseService;
+
+public interface IDenunciaEstadoService extends IBaseService<DenunciaEstadoDTO, DenunciaEstadoResult> {
+}

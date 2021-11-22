e15fca81364f31822ad8b3af977022b80d77b401
DalilaC33 <dalila.castelnovo@fiuni.edu.py>
2021-11-22 19:52:56 -0300

    cambios_casos_derESTE_ES

--- src/main/java/com/protectionapp/sd2021/controller/CasosDerivadosResource.java
+++ src/main/java/com/protectionapp/sd2021/controller/CasosDerivadosResource.java
@@ -3,10 +3,13 @@ package com.protectionapp.sd2021.controller;
 
 import com.protectionapp.sd2021.dto.casosDerivados.CasosDerivadosDTO;
 import com.protectionapp.sd2021.dto.casosDerivados.CasosDerivadosResult;
+import com.protectionapp.sd2021.dto.casosDerivados.DepEstadoDTO;
+import com.protectionapp.sd2021.dto.casosDerivados.DepEstadoResult;
 import com.protectionapp.sd2021.dto.user.UserDTO;
 import com.protectionapp.sd2021.service.casosDerivados.CasosDerivadosServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,15 +35,24 @@ public class CasosDerivadosResource {
         return cDService.getAll(PageRequest.of(pageNum, 5));
     }
 
+    @GetMapping(path = "/")
+    public CasosDerivadosResult getAll(Pageable page) {
+        return cDService.getAll(page);
+    }
+
+
     @PostMapping()
     public CasosDerivadosDTO save(@Valid @RequestBody CasosDerivadosDTO cDDto) {
         return cDService.save(cDDto);
     }
 
-
+    @DeleteMapping("/{id}")
+    public CasosDerivadosDTO delete(@PathVariable(value = "id") Integer id) {
+        return cDService.delete(id);
+    }
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public CasosDerivadosDTO updateCasosDerivados(@RequestBody CasosDerivadosDTO casoDerivadosDTO, @PathVariable(value = "id") Integer id) {
+    public CasosDerivadosDTO update(@RequestBody CasosDerivadosDTO casoDerivadosDTO, @PathVariable(value = "id") Integer id) {
         return cDService.update(casoDerivadosDTO, id);
 
     }

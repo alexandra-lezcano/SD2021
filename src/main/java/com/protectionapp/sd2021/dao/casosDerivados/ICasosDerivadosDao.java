023c9b5d7a81e843420032b2f2cc9c50efe49fe8
DalilaC33 <dalila.castelnovo@fiuni.edu.py>
2021-11-22 19:40:50 -0300

    cambiosCasosDerivados

--- src/main/java/com/protectionapp/sd2021/dao/casosDerivados/ICasosDerivadosDao.java
+++ src/main/java/com/protectionapp/sd2021/dao/casosDerivados/ICasosDerivadosDao.java
@@ -4,9 +4,10 @@ import com.protectionapp.sd2021.domain.casosDerivados.CasosDerivadosDomain;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ICasosDerivadosDao extends CrudRepository<CasosDerivadosDomain, Integer> {
+public interface ICasosDerivadosDao extends JpaRepository<CasosDerivadosDomain, Integer> {
     public Page<CasosDerivadosDomain> findAll(Pageable pageable);
 
 }
